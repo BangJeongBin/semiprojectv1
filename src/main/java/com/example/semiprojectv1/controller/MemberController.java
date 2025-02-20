e71af622bf9267b5b2d3ddbfe858adfa74eb0ec1
41817 <bangjungbin@gmail.com>
2025-02-20 16:47:17 +0900

    nav에 정의된 링크에 맞게 컨트롤러와 엔드포인트 정의 엔드포인트에 따른 뷰 페이지도 지정

---
+++ src/main/java/com/example/semiprojectv1/controller/MemberController.java
@@ -0,0 +1,25 @@
+package com.example.semiprojectv1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member") /* 이렇게 안쓴다면 아래에서 적용하면 됨 */
+public class MemberController {
+
+    @GetMapping("/join")
+    public String join() {
+        return "views/member/join";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "views/member/login";
+    }
+
+    @GetMapping("/myinfo")
+    public String myinfo() {
+        return "views/member/myinfo";
+    }
+}
