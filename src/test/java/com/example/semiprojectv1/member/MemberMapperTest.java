646234af003ba5141c94b555cda352eeb3b94e87
41817 <bangjungbin@gmail.com>
2025-02-21 12:13:31 +0900

    MemberRepository 테스트 시 생성자 주입방식 사용

--- src/test/java/com/example/semiprojectv1/member/MemberMapperTest.java
+++ src/test/java/com/example/semiprojectv1/member/MemberMapperTest.java
@@ -2,20 +2,24 @@ package com.example.semiprojectv1.member;
 
 import com.example.semiprojectv1.domain.MemberDTO;
 import com.example.semiprojectv1.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.TestConstructor;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 @MybatisTest
+@RequiredArgsConstructor    // final 필드변수로 생성자로 생성 (final 변수를 쓰기 위한 애노테이션)
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)   // 생성자 주입 시 필요
 public class MemberMapperTest {
 
-    @Autowired
-    private MemberRepository memberMapper;
+    // @Autowired보다 final을 사용해 생성자를 이용한 의존성 주입이 더 좋음
+    private final MemberRepository memberMapper;
 
     @Test
     @DisplayName("MemberMapper Insert test")
