b32743d638d87cef8f1b36d2c5e50c14ce71ea0f
41817 <bangjungbin@gmail.com>
2025-02-21 11:25:02 +0900

    회원가입 유효성검사 2 - addEventListener 방식 MemberDTO 작성 submit한 회원정보를 컨트롤러에서 로그로 확인

--- src/main/java/com/example/semiprojectv1/controller/MemberController.java
+++ src/main/java/com/example/semiprojectv1/controller/MemberController.java
@@ -1,10 +1,13 @@
 package com.example.semiprojectv1.controller;
 
+import com.example.semiprojectv1.domain.MemberDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j  // lombok
 @Controller
 @RequestMapping("/member") /* 이렇게 안쓴다면 아래에서 적용하면 됨 */
 public class MemberController {
@@ -15,7 +18,9 @@ public class MemberController {
     }
 
     @PostMapping("/join")
-    public String joinok() {
+    public String joinok(MemberDTO member) {
+        log.info("submit된 회원정보 : {}", member);  // 넘어온 값을 확인하는 코드 - lombok 사용
+
         return "redirect:/member/login";
     }
 
---
+++ src/main/java/com/example/semiprojectv1/domain/MemberDTO.java
@@ -0,0 +1,13 @@
+package com.example.semiprojectv1.domain;
+
+import lombok.Data;
+
+@Data   // setter, getter, toString 자동 생성
+public class MemberDTO {
+
+    private String userid;
+    private String passwd;
+    private String repasswd;
+    private String name;
+    private String email;
+}
