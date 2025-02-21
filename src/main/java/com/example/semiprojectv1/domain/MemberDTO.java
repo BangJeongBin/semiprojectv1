ba228feddfacb47824442cd104fc1e6424e623e3
41817 <bangjungbin@gmail.com>
2025-02-21 11:50:45 +0900

    MemberRepository 작성 MemberDTO builder 패턴 추가 MemberRepository 테스트 작성

--- src/main/java/com/example/semiprojectv1/domain/MemberDTO.java
+++ src/main/java/com/example/semiprojectv1/domain/MemberDTO.java
@@ -1,8 +1,10 @@
 package com.example.semiprojectv1.domain;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data   // setter, getter, toString 자동 생성
+@Builder
 public class MemberDTO {
 
     private String userid;
