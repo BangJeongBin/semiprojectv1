e8676d3290a9a853963a471de99fd99d232461e0
JeongBin Bang <bangjungbin@gmail.com>
2025-02-23 16:35:07 +0900

    주석 작성

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
 -- members 테이블
+-- server를 재시작 할 때마다 dummy data가 생성됨
 insert into members (userid, passwd, name, email) values
 ('abc123', 'abc123', 'abc123', 'abc123@abc123co.kr'),
 ('987xyz', '987xyz', '987xyz', '987xyz@987xyz.co.kr');
