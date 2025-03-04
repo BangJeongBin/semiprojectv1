4463fdf38f603971c7bd57e6ff8865009cb71695
41817 <bangjungbin@gmail.com>
2025-03-04 10:33:08 +0900

    게시판 글쓰기 구현 1 - 글쓰기 폼 작성 google recapcha 추가 tinyMCE 추가

--- src/main/java/com/example/semiprojectv1/controller/BoardController.java
+++ src/main/java/com/example/semiprojectv1/controller/BoardController.java
@@ -62,4 +62,11 @@ public class BoardController {
 
         return "views/board/view";
     }
+
+
+    @GetMapping("/write")
+    public String write() {
+
+        return "views/board/write";
+    }
 }
