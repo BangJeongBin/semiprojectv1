ea3b6c6c67ca7d9c61589d3bc329cb6ad701a0c2
41817 <bangjungbin@gmail.com>
2025-03-06 14:14:08 +0900

    갤러리 구현 3 - 갤러리 목록 출력 정식 구현

--- src/main/java/com/example/semiprojectv1/controller/GalleryController.java
+++ src/main/java/com/example/semiprojectv1/controller/GalleryController.java
@@ -1,15 +1,25 @@
 package com.example.semiprojectv1.controller;
 
+import com.example.semiprojectv1.service.GalleryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/gallery")
+@RequiredArgsConstructor
 public class GalleryController {
 
+    private final GalleryService galleryService;
+
+
     @GetMapping("/list")
-    public String list() {
+    public String list(Model m) {
+
+        m.addAttribute("gals", galleryService.selectGallery());
+
         return "views/gallery/list";
     }
 }
