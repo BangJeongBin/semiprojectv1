11cc0c22538be4fd63a1b853b939d4ae659b63fa
41817 <bangjungbin@gmail.com>
2025-03-07 10:43:22 +0900

    갤러리 구현 4 - 갤러리 본문 출력 구현 본문글 URL 지정은 경로매개변수 사용 본문글, 첨부이미지를 위한 DTO 정의

--- src/main/java/com/example/semiprojectv1/service/GalleryService.java
+++ src/main/java/com/example/semiprojectv1/service/GalleryService.java
@@ -1,10 +1,14 @@
 package com.example.semiprojectv1.service;
 
-import com.example.semiprojectv1.domain.Gallery;
+import com.example.semiprojectv1.domain.GalleryImageDTO;
+import com.example.semiprojectv1.domain.GalleryListDTO;
+import com.example.semiprojectv1.domain.GalleryViewDTO;
 
 import java.util.List;
 
 public interface GalleryService {
 
-    List<Gallery> selectGallery();
+    List<GalleryListDTO> selectGallery();
+
+    GalleryImageDTO readOneGalleryImage(int gno);
 }
