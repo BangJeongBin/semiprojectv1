0fdf6d2e02211826c80154ac6602fbc38a3be596
41817 <bangjungbin@gmail.com>
2025-03-10 10:10:13 +0900

    갤러리 구현 8 - 갤러리 글쓰기 등록 구현 2 이미지 업로드 관련 라이브러리 추가 첨부된 파일을 업로드 처리하고 관련정보를 gallery_images에 저장

--- src/main/java/com/example/semiprojectv1/service/GalleryServiceImpl.java
+++ src/main/java/com/example/semiprojectv1/service/GalleryServiceImpl.java
@@ -16,6 +16,7 @@ import java.util.List;
 public class GalleryServiceImpl implements GalleryService {
 
     private final GalleryRepository galleryMapper;
+    private final GalleryUploadService galleryUploadService;
 
 
     @Override
@@ -45,6 +46,16 @@ public class GalleryServiceImpl implements GalleryService {
         }
 
         // 첨부된 파일을 업로드 처리하고 가져온 글 번호로 첨부된 파일에 대한 정보를 gallery_images에 저장
+        if (!ginames.isEmpty()) {   // 첨부파일이 존재한다면
+            // 업로드 처리 후 업로드된 파일들의 정보를 리스트 형태로 받아옴.
+            List<NewGalleryImageDTO> gis = galleryUploadService.processUpload(ginames, gno);
+
+            // 업로드 된 파일의 정보를 gallery_images 테이블에 저장
+            // 즉, 첨부 된 파일정보를 개별 행으로 저장
+            for (NewGalleryImageDTO gi : gis) {
+                galleryMapper.insertGalleryImage(gi);
+            }
+        }
 
         // 첨부된 파일들 중 첫번째 이미지 파일을 썸네일 처리
 
