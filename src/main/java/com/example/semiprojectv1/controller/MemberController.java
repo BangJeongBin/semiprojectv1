dd859def393af8fe085c4fb2aa8170489019081a
41817 <bangjungbin@gmail.com>
2025-04-01 10:15:30 +0900

    로그인 오류, 성공 메새지 구현

--- src/main/java/com/example/semiprojectv1/controller/MemberController.java
+++ src/main/java/com/example/semiprojectv1/controller/MemberController.java
@@ -2,10 +2,14 @@ package com.example.semiprojectv1.controller;
 
 import com.example.semiprojectv1.domain.Member;
 import com.example.semiprojectv1.domain.MemberDTO;
+import com.example.semiprojectv1.jwt.JwtTokenProvider;
 import com.example.semiprojectv1.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -14,6 +18,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Slf4j  // lombok - logging객체 자동 생성
@@ -23,6 +30,8 @@ import javax.servlet.http.HttpSession;
 public class MemberController {
 
     private final MemberService memberService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider jwtTokenProvider;
 
 
     @GetMapping("/join")
@@ -65,38 +74,46 @@ public class MemberController {
 
 
     // 스프링 시큐리티가 자동으로 처리 - 생략
-//    @PostMapping("/login")
-//    public ResponseEntity<?> loginok(MemberDTO member, HttpSession session) {
-//        // 로그인 처리 시 기타오류 발생에 대한 응답코드 설정
-//        ResponseEntity<?> response = ResponseEntity.internalServerError().build();
-//
-//        log.info("submit된 회원정보 : {}", member);  // 넘어온 값을 확인하는 코드 - lombok 사용
-//
-//        try {
-//            // 정상적으로 처리되는 경우 상태코드 200으로 응답
-//            Member loginUser = memberService.loginMember(member);
-//            session.setAttribute("loginUser", loginUser);
-//            session.setMaxInactiveInterval(600);    // 세션 유지 : 10분
-//
-//            response =  ResponseEntity.ok().build();
-//            System.out.println("==================" + response);
-//        } catch (IllegalStateException e) {
-//            // 비정상 처리 시 상태코드 400으로 응답 - 클라이언트 쟐못
-//            // 아이디나 비밀번호 잘못 입력 시
-//            response = ResponseEntity.badRequest().body(e.getMessage());
-//            e.printStackTrace();
-//        } catch (Exception e) {
-//            // 비정상 처리 시 상태코드 500으로 응답 - 서버 쟐못
-//            e.printStackTrace();
-//        }
-//
-//        return response;
-//    }
+    @PostMapping("/login")
+    public ResponseEntity<?> loginok(MemberDTO member, HttpServletResponse res) {
+        // 로그인 처리 시 기타오류 발생에 대한 응답코드 설정
+        ResponseEntity<?> response = ResponseEntity.internalServerError().build();
+
+        log.info("submit된 회원정보 : {}", member);  // 넘어온 값을 확인하는 코드 - lombok 사용
+
+        try {
+            // 스프링 시큐리티에서 제공하는 인증처리 메니저로 인증 처리
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(member.getUserid(), member.getPasswd()));
+
+            // 인증이 완료되면 JWT 토큰 생성
+            final String jwt = jwtTokenProvider.generateToken(member.getUserid());
+
+            // JWT 토큰을 쿠키에 저장
+            Cookie cookie = new Cookie("jwt", jwt);
+            cookie.setHttpOnly(true); // 토큰은 header를 통해서만 서버로 전송가능
+            cookie.setMaxAge(60 * 30); // 유효시간 30분
+            cookie.setPath("/");
+            res.addCookie(cookie);
+
+            response =  ResponseEntity.ok().body("로그인 성공했습니다.");
+            System.out.println("==================" + response);
+        } catch (IllegalStateException e) {
+            // 비정상 처리 시 상태코드 400으로 응답 - 클라이언트 쟐못
+            // 아이디나 비밀번호 잘못 입력 시
+            response = ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인에 실패했습니다.");
+            e.printStackTrace();
+        } catch (Exception e) {
+            // 비정상 처리 시 상태코드 500으로 응답 - 서버 쟐못
+            e.printStackTrace();
+        }
+
+        return response;
+    }
 
 
     @GetMapping("/myinfo")
     public String myinfo(Authentication authentication, Model model) {
-        String returnUrl = "views/member/login";
+        String returnUrl = "redirect:/member/login";
 
         // 로그인 인증이 성공했다면
         if (authentication != null && authentication.isAuthenticated()) {
@@ -111,12 +128,21 @@ public class MemberController {
     }
 
 
-    // 스프링 시큐리티가 자동으로 처리 - 생략
-//    @GetMapping("/logout")
-//    public String logout(HttpSession session) {
-//        session.invalidate();   // 세션 제거
-//        return "redirect:/";
-//    }
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest req, HttpServletResponse res) {
+        // 쿠키에서 JWT 삭제
+        Cookie cookie = new Cookie("jwt", null);
+        cookie.setMaxAge(0);
+        cookie.setPath("/");
+        res.addCookie(cookie);
+
+        // 세션 무효화
+        HttpSession session = req.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        return "redirect:/";
+    }
 
 
     @GetMapping("/loginfail")
